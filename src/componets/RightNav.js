8b68080f06dac3d019132270c6fb4111844620fe
Adrian Carlson <adrian.r.carlson@gmail.com>
2022-08-11 15:44:44 -0500

    fixing welcome page error after right nav

--- src/componets/RightNav.js
+++ src/componets/RightNav.js
@@ -3,25 +3,36 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { selectArticleByName } from '../features/articles/articlesSlice';
 import SectionsList from '../features/sections/SectionsList';
-
 import { deFormatRoute } from '../utils/formatRoute';
+
 const RightNav = () => {
 	const { name: articlePath } = useParams();
-	const { name: articleName, id: articleId } = useSelector(selectArticleByName(deFormatRoute(articlePath)));
+	const article = useSelector(selectArticleByName(deFormatRoute(articlePath)));
+
+	if (article) {
+		const { name: articleName, id: articleId } = article;
+		return (
+			<Col id='pd-side-bar' lg='2' className='d-none d-lg-block border-start'>
+				<Container fluid className='sticky-top sticky-offset-sidebar'>
+					<Row className='mt-4 ms-2 me-2'>
+						<h6 className='border-bottom mb-0 pb-1'>On this Page</h6>
+						<ListGroup variant='flush' className='small'>
+							<ListGroup.Item action variant='light' className='rounded-2 border-bottom-0'>
+								{articleName}
+							</ListGroup.Item>
+							<SectionsList articleId={articleId} />
+						</ListGroup>
+					</Row>
+				</Container>
+			</Col>
+		);
+	}
 	return (
-		<Col id='pd-side-bar' lg='2' className='d-none d-lg-block border-start'>
-			<Container fluid className='sticky-top sticky-offset-sidebar'>
-				<Row className='mt-4 ms-2 me-2'>
-					<h6 className='border-bottom mb-0 pb-1'>On this Page</h6>
-					<ListGroup variant='flush' className='small'>
-						<ListGroup.Item action variant='light' className='rounded-2 border-bottom-0'>
-							{articleName}
-						</ListGroup.Item>
-						<SectionsList articleId={articleId} />
-					</ListGroup>
-				</Row>
-			</Container>
-		</Col>
+		<>
+			<Col id='pd-side-bar' lg='2' className='d-none d-lg-block border-start'>
+				<Container fluid className='sticky-top sticky-offset-sidebar'></Container>
+			</Col>
+		</>
 	);
 };
 export default RightNav;
--- src/features/sections/SectionsList.js
+++ src/features/sections/SectionsList.js
@@ -3,14 +3,13 @@ import { selectSectionsByArticleId } from './sectionsSlice';
 import { ListGroup } from 'react-bootstrap';
 const SectionsList = ({ articleId }) => {
 	const sections = useSelector(selectSectionsByArticleId(articleId));
-	console.log(sections);
 	if (sections && sections.length > 0) {
 		return (
 			<>
-				{sections.map((section) => {
+				{sections.map((section, index) => {
 					if (section.header) {
 						return (
-							<ListGroup.Item action variant='light' className='rounded-2 border-bottom-0'>
+							<ListGroup.Item key={index} action variant='light' className='rounded-2 border-bottom-0'>
 								{section.header}
 							</ListGroup.Item>
 						);
