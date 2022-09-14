2f59c73cb8481aa7f6094e972e245ae2e1de316b
Adrian Carlson <adrian.r.carlson@gmail.com>
2022-09-14 09:12:29 -0500

    Finish hash link

--- src/features/sections/SectionsList.js
+++ src/features/sections/SectionsList.js
@@ -1,17 +1,26 @@
 import { useSelector } from 'react-redux';
 import { selectSectionsByArticleId } from './sectionsSlice';
 import { formatRoute } from '../../utils/formatRoute';
+import { HashLink } from 'react-router-hash-link';
+import { selectArticleById } from '../articles/articlesSlice';
+
 const SectionsList = ({ articleId }) => {
+	const article = useSelector(selectArticleById(articleId));
 	const sections = useSelector(selectSectionsByArticleId(articleId));
+	
 	if (sections && sections.length > 0) {
 		return (
 			<>
 				{sections.map((section, index) => {
 					if (section.header) {
 						return (
-							<a href={`#${formatRoute(section.header)}`} class='list-group-item list-group-item-action list-group-item-light rounded-2 border-bottom-0' key={index}>
+							<HashLink
+								to={`/${formatRoute(article.category)}/${formatRoute(article.name)}#${formatRoute(section.header)}`}
+								key={index}
+								className='list-group-item list-group-item-action list-group-item-light rounded-2 border-bottom-0'
+							>
 								{section.header}
-							</a>
+							</HashLink>
 						);
 					}
 					return <div key='-1'></div>;
